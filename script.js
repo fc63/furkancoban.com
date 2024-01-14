9213138a6b65b373541b179542f1e10202feda47
Furkan Çoban <139001749+fc63@users.noreply.github.com>
2024-01-14 12:07:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 var modal = document.getElementById("myModal");
-      var img = document.getElementById("myImg");
-      var modalImg = document.getElementById("img01");
-      img.onclick = function(){
-        modal.style.display = "block";
-        modalImg.src = this.src;
-      }
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+img.onclick = function() {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+}
 
-      var span = document.getElementsByClassName("close")[0];
-      span.onclick = function() {
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function() {
+    modal.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
         modal.style.display = "none";
-      }
+    }
+}
